9659894e7f0ff5178c0b3d045eaa5ba85f5678df
balavikneshram.j <balaram0711@gmail.com>
2026-06-14 20:06:30 +0530

    for head beat to keep alive

--- anime-web/src/main/java/com/bu/anime_web/controller/AnimeController.java
+++ anime-web/src/main/java/com/bu/anime_web/controller/AnimeController.java
@@ -9,10 +9,7 @@ import com.bu.anime_web.vo.Response.RecentAnimeResponseVO;
 import com.bu.anime_web.vo.common.AnimeVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -30,5 +27,9 @@ public class AnimeController {
     @PostMapping("/fetchAnime")
     public ResponseEntity<AnimeVO> fetchAnime(@RequestBody AnimeRequestVO animeRequestVO) {
         return ResponseEntity.ok(animeService.fetchAnime(animeRequestVO));
+    }
+    @GetMapping("/heartBeat")
+    public ResponseEntity<String> heartBeat() {
+        return ResponseEntity.ok("website is live");
     }
 }
